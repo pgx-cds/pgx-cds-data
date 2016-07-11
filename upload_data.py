b1a7225349b7106299fb33e9644f852ed72fc376
Gaston Fiore <gaston.fiore@gmail.com>
2016-07-11 16:52:15 -0400

    Upload all synthetic data

--- upload_data.py
+++ upload_data.py
@@ -10,18 +10,63 @@ patient_ids = ['smart-1288992', 'smart-1032702', 'smart-1081332',
     'smart-1869612', 'smart-1951076', 'smart-2004454', 'smart-2042917',
     'smart-2080416', 'smart-2081539', 'smart-2113340', 'smart-2169591']
 
+# Upload performer data
+performer_path = ('/Users/gafiore/code/pgx-cds/examples/organizations/'
+                  'acme-lab.json')
+with open(performer_path, 'r') as f:
+    performer = json.load(f)
+performer_url = ('http://pgx-fhir.smarthealthit.org:8080/baseDstu2/' +
+                 performer['resourceType'] + '/' +
+                 performer['id'])
+performer_req = urllib.request.Request(url=performer_url,
+    data=json.dumps(performer).encode('utf-8'),
+    headers={'Content-Type': 'application/json'}, method='PUT')
+with urllib.request.urlopen(performer_req) as f:
+    print(f.read().decode('utf-8'))
+print(f.status)
+print(f.reason)
+
 patients = []
+genotypes = []
+phenotypes = []
+tpmt_path = '/Users/gafiore/code/pgx-cds/examples/tpmt/'
 for id in patient_ids:
     patients.append({'resourceType': 'Patient', 'id': id})
+    genotype_path = tpmt_path + 'Observation-tpmtgenotype-' + id[6:] + '.json'
+    with open(genotype_path, 'r') as f:
+        genotypes.append(json.load(f))
+    phenotype_path = tpmt_path + 'Observation-tpmtphenotype-' + id[6:] + '.json'
+    with open(phenotype_path, 'r') as f:
+        phenotypes.append(json.load(f))
 
-for patient in patients:
-    url = ('http://pgx-fhir.smarthealthit.org:8080/baseDstu2/' +
-           patient['resourceType'] + '/' +
-           patient['id'])
-    req = urllib.request.Request(url=url,
-    data=json.dumps(patient).encode('utf-8'),
-    headers={'Content-Type': 'application/json'}, method='PUT')
-    with urllib.request.urlopen(req) as f:
+for patient, genotype, phenotype in zip(patients, genotypes, phenotypes):
+    patient_url = ('http://pgx-fhir.smarthealthit.org:8080/baseDstu2/' +
+                   patient['resourceType'] + '/' +
+                   patient['id'])
+    patient_req = urllib.request.Request(url=patient_url,
+        data=json.dumps(patient).encode('utf-8'),
+        headers={'Content-Type': 'application/json'}, method='PUT')
+    with urllib.request.urlopen(patient_req) as f:
+        print(f.read().decode('utf-8'))
+    print(f.status)
+    print(f.reason)
+    genotype_url = ('http://pgx-fhir.smarthealthit.org:8080/baseDstu2/' +
+                    genotype['resourceType'] + '/' +
+                    genotype['id'])
+    genotype_req = urllib.request.Request(url=genotype_url,
+        data=json.dumps(genotype).encode('utf-8'),
+        headers={'Content-Type': 'application/json'}, method='PUT')
+    with urllib.request.urlopen(genotype_req) as f:
+        print(f.read().decode('utf-8'))
+    print(f.status)
+    print(f.reason)
+    phenotype_url = ('http://pgx-fhir.smarthealthit.org:8080/baseDstu2/' +
+                     phenotype['resourceType'] + '/' +
+                     phenotype['id'])
+    phenotype_req = urllib.request.Request(url=phenotype_url,
+        data=json.dumps(phenotype).encode('utf-8'),
+        headers={'Content-Type': 'application/json'}, method='PUT')
+    with urllib.request.urlopen(phenotype_req) as f:
         print(f.read().decode('utf-8'))
     print(f.status)
     print(f.reason)
